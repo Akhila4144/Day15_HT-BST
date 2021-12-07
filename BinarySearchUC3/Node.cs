b033be450109ecc712a61272ef6acc85149ba375
Akhila4144 <orugantiakhila34@gmail.com>
2021-12-07 13:21:01 +0530

    UC3 Done

---
+++ BinarySearchUC3/Node.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BinarySearchUC3
+{
+    public class Node
+    {
+        public Node LeftNode { get; set; }
+        public Node RightNode { get; set; }
+        public int data { get; set; }
+    }
+}
