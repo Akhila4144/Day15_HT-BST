57d7094d3919a1aece8c977a7109b4b005cf19f5
Akhila4144 <orugantiakhila34@gmail.com>
2021-12-07 13:05:31 +0530

    UC2 Done

---
+++ HashTableUC2/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace HashTableUC2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyMapNode<string, string> hash = new MyMapNode<string, string>(18);
+            hash.Add("0", "Paranoids");
+            hash.Add("1", "are");
+            hash.Add("2", "not");
+            hash.Add("3", "paranoid");
+            hash.Add("4", "because");
+            hash.Add("5", "they");
+            hash.Add("6", "are");
+            hash.Add("7", "paranoid");
+            hash.Add("8", "but ");
+            hash.Add("9", "because");
+            hash.Add("10", "they");
+            hash.Add("11", "keep");
+            hash.Add("12", "putting");
+            hash.Add("13", "themselves");
+            hash.Add("14", "delibertely");
+            hash.Add("15", "into");
+            hash.Add("16", "paranoid");
+            hash.Add("17", "avoidable");
+            hash.Add("18", "situations");
+
+            string hash8 = hash.Get("8");
+            Console.WriteLine("8th index value:" + hash8);
+            string hash14 = hash.Get("14");
+            Console.WriteLine("14th index value:" + hash14);
+
+
+        }
+    }
+}
