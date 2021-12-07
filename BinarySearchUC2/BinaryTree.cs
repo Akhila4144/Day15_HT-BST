609038df32e8e9d6ab569086968d002673d51d71
Akhila4144 <orugantiakhila34@gmail.com>
2021-12-07 13:18:01 +0530

    UC2 Done

---
+++ BinarySearchUC2/BinaryTree.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+namespace BinarySearchUC2
+{
+    public class BinaryTree
+    {
+        public Node root = null;
+
+        public void Insert(int i)
+        {
+            Node newNode = new Node();
+            newNode.data = i;
+            if (root == null)
+            {
+                root = newNode;
+            }
+            else
+            {
+                Node current = root;
+                Node parent;
+                while (true)
+                {
+                    parent = current;
+                    if (i < current.data)
+                    {
+                        parent.LeftNode = newNode;
+                        break;
+                    }
+
+                    else
+                    {
+                        current = current.RightNode;
+                        if (current == null)
+                        {
+                            parent.RightNode = newNode;
+                            break;
+                        }
+                    }
+
+                }
+
+            }
+        }
+
+
+            public void TraverseInorder(Node root)
+            {
+                if (root == null)
+                {
+                    return;
+                }
+                TraverseInorder(root.LeftNode);
+                Console.Write(root.data + "");
+                TraverseInorder(root.RightNode);
+            }
+
+            public void TravesePreorder(Node root)
+            {
+                if (root == null)
+                {
+                    return;
+                }
+                Console.WriteLine(root.data + "");
+                TravesePreorder(root.LeftNode);
+                TravesePreorder(root.RightNode);
+            }
+            public void TraversePostorder(Node root)
+            {
+                if (root == null)
+                {
+                    return;
+                }
+                TraversePostorder(root.LeftNode);
+                TraversePostorder(root.RightNode);
+                Console.WriteLine(root.data + "");
+            }
+        }
+    }
+
+             
+           
---
+++ BinarySearchUC2/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace BinarySearchUC2
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            BinaryTree obj1 = new BinaryTree();
+            obj1.Insert(56);
+            obj1.Insert(30);
+            obj1.Insert(70);
+            obj1.Insert(22);
+            obj1.Insert(40);
+            obj1.Insert(60);
+            obj1.Insert(95);
+            obj1.Insert(11); 
+            obj1.Insert(65);
+            obj1.Insert(3);
+            obj1.Insert(16);
+            obj1.Insert(63);
+            obj1.Insert(67);
+            obj1.TraverseInorder(obj1.root);
+            Console.WriteLine();
+            obj1.TraversePostorder(obj1.root);
+            Console.WriteLine();
+            obj1.TravesePreorder(obj1.root);
+            Console.WriteLine();
+
+        }
+    }
+}
